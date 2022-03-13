4cccd6a25180dbef2b2df0a4c9e28f1dfa16d776
Mostafa Hammouda <88328149+MostafaHammouda@users.noreply.github.com>
2022-03-14 01:32:27 +0200

    Add files via upload

---
+++ ServiceHost/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.IO;
+
+namespace HiServiceHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(trial5.Service1)))
+            {
+                host.Open();
+                {
+                    string csvFile = "Name_Date_DB.csv";
+                    string searchString = Console.ReadLine();
+                    char csvSeparator = ',';
+
+                    foreach (string line in File.ReadLines(csvFile))
+                        foreach (string value in line.Replace("\"", "").Split('\r', '\n', csvSeparator))
+
+                           if (value.Trim() == searchString.Trim())
+                            {
+                                Console.WriteLine("Name Found");
+                                goto AfterLoop;
+
+                            }
+                    Console.WriteLine("Name Not Found");
+                AfterLoop:
+                            Console.WriteLine("program works");
+
+                    
+
+                }
+
+            }
+        }
+    }
+}
